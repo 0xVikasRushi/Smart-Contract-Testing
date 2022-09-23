f5bd1144c4315069329e226cf8d810b92386d817
vikasrushi <0xvikas@gmail.com>
2022-09-23 23:25:08 +0530

    refactor truffle tested contracts

--- contracts/Even.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.5.0 <0.9.0;
-
-contract Even {
-    function checkEven(uint256 a) public pure returns (uint256) {
-        require(a % 2 == 0, "a is odd");
-        return a;
-    }
-}
